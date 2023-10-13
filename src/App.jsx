d086df8f6e7826e04b3d0c03c963b4da100d27d2
amitay <amitay1599@gmail.com>
2023-10-14 01:53:27 +0300

    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import ImageEmojiEditor from "./pages/ImageEmojiEditor";
+import ReactGA from "react-ga4";
 
 function App() {
+  ReactGA.initialize("G-V73GP237ET");
   return <ImageEmojiEditor />;
 }
 
--- src/pages/ImageEmojiEditor.jsx
+++ src/pages/ImageEmojiEditor.jsx
@@ -2,7 +2,6 @@ import React, { useRef, useState, useEffect } from "react";
 import overlayImage from "../assets/Israel.png";
 import rachel from "../assets/rachel.png";
 
-
 const ImageTextEditor = () => {
   const fileInputRef = useRef(null);
   const canvasRef = useRef(null);
@@ -27,14 +26,14 @@ const ImageTextEditor = () => {
     const overlayImg = new Image();
 
     img.onload = () => {
-      canvas.width = 170;
-      canvas.height = 170;
-      ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, 170, 170);
+      canvas.width = 240;
+      canvas.height = 240;
+      ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, 240, 240);
 
       overlayImg.onload = () => {
-        const overlayX = (canvas.width - overlayImg.width) / 2;
-        const overlayY = 10; // Padding from the top, adjust as needed
-        ctx.drawImage(overlayImg, overlayX, overlayY);
+        const paddingX = 10; // Padding from the left
+        const paddingY = 10; // Padding from the top
+        ctx.drawImage(overlayImg, paddingX, paddingY);
 
         // Set the text settings
         ctx.fillStyle = "white";
@@ -86,37 +85,51 @@ const ImageTextEditor = () => {
   };
 
   return (
-    <div className="p-4 space-y-4 grid place-content-center py-24 border border-black min-h-screen">
-      <h1 className="text-center">מוקדש לרחל ❤️</h1>
-      <img src={rachel} className="h-72 w-72 mx-auto" />
+    <div className="flex flex-col items-center justify-center min-h-screen p-24 border border-black space-y-4">
+      <h1 className="text-center text-2xl font-bold">מוקדש לרחל ❤️</h1>
 
-      <input
-        type="file"
-        ref={fileInputRef}
-        onChange={handleImageUpload}
-        className="p-2 border border-gray-300 rounded"
+      <img
+        src={rachel}
+        alt="Rachel's Portrait"
+        className=" h-60 w-60"
       />
-      <div>
+
+      <div className="w-full max-w-md">
+        <input
+          type="file"
+          ref={fileInputRef}
+          onChange={handleImageUpload}
+          className="w-full p-2 border border-gray-300 rounded"
+          accept="image/*"
+        />
+      </div>
+
+      <div className="w-full max-w-md">
         <label className="block text-sm font-medium text-gray-700">
           Add Text:
           <input
             type="text"
             value={text}
             onChange={handleTextChange}
-            className="mt-1 p-2 w-full border border-gray-300 rounded"
+            className="mt-1 w-full p-2 border border-gray-300 rounded"
+            placeholder="Type your text here..."
           />
         </label>
       </div>
+
       {imageSrc && (
-        <>
-          <canvas ref={canvasRef} className="border border-gray-300" />
+        <div className="w-full max-w-md flex flex-col items-center space-y-4">
+          <canvas
+            ref={canvasRef}
+            className="w-full border border-gray-300 rounded"
+          />
           <button
             onClick={handleDownload}
-            className="mt-2 px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600"
+            className="w-full px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600"
           >
             Download Image
           </button>
-        </>
+        </div>
       )}
     </div>
   );
